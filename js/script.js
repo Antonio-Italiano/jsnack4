38f4688d6b97200b724d430e8331135337e151c0
Antonio-Italiano <antoitalia221@gmail.com>
2023-01-04 16:41:58 +0100

    Add array persons

--- js/script.js
+++ js/script.js
@@ -5,4 +5,18 @@ Crea quindi un nuovo array inserendo, per ogni persona, una frase con il
 nome e cognome e l’indicazione se può guidare, in base all’età
 */
 
+// CREO ARRAY
+const persons = [
+
+    { firstname:'Leone',      lastname: 'Felidi',    age: 75 },
+    { firstname:'Giacomo',    lastname: 'Andreotti', age: 54 },
+    { firstname:'Vincenzo',   lastname: 'Torre',     age: 7 },
+    { firstname:'Antonio',    lastname: 'Italiano',  age: 22 },
+    { firstname:'Roberto',    lastname: 'Lipari',    age: 32 },
+    { firstname:'Vincenzino', lastname: 'Guidi',     age: 8 },
+    { firstname:'Melanie',    lastname: 'Ferrari',   age: 14 },
+    { firstname:'Giuseppe',   lastname: 'Russo',     age: 19 },
+    
+];
+
 
